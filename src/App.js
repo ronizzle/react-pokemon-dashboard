9269197c118b728853a9671a49e66a0c722198f4
Roniel Bernas <roniel.bernas@crmworksa.asia>
2017-04-01 17:51:00 +0800

    used react strap pagination component to implement pagination on the pokedesk app

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
 import PokeList from './components/PokeList'
-import { Col } from 'react-bootstrap/lib/'
+import { Col, Pagination } from 'react-bootstrap/lib/'
 
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            pokemons: []
+            pokemons: [],
+            activePage: 0,
+            limit: 50,
+            offset: 0,
+            totalPages: 0
         }
+        this.handlePaginationSelect = this.handlePaginationSelect.bind(this)
         this.loadPokemons = this.loadPokemons.bind(this)
+    }
+
+    handlePaginationSelect(event) {
+        let offset = this.state.limit * (event - 1)
+        this.loadPokemons(`${this.props.baseUrl}pokemon?limit=${this.state.limit}&offset=${offset}`)
     }
 
     loadPokemons(url) {
@@ -19,16 +29,24 @@ class App extends Component {
             .then(response => {
                 return response.json()
             }).then(json => {
+
+                console.log(json)
+
+                let pages = Math.round(json.count / this.state.limit)
+
                 this.setState({
-                    pokemons: json.results
+                    pokemons: json.results,
+                    totalPages: pages,
+                    count: json.count
                 })
+
             }).catch(err => {
                 console.log(err)
         })
     }
 
     componentWillMount() {
-        this.loadPokemons('http://pokeapi.co/api/v2/pokemon')
+        this.loadPokemons(`${this.props.baseUrl}pokemon?limit=${this.state.limit}&offset=0`)
     }
 
     render() {
@@ -41,6 +59,15 @@ class App extends Component {
 
                 <Col sm={8} md={10} smOffset={2} mdOffset={2}>
                     <PokeList pokemons={this.state.pokemons} />
+                </Col>
+
+                <Col sm={12}>
+                    <Pagination
+                        bsSize="small"
+                        items={this.state.totalPages}
+                        activePage={this.state.activePage}
+                        onSelect={this.handlePaginationSelect}
+                    />
                 </Col>
             </div>
         );
